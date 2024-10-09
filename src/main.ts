828f033c7f6d0461b0356eb107401980378f630b
Arjun Krishnan <akrish29@ucsc.edu>
2024-10-08 22:24:40 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,38 +5,61 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Mosquito Swatting Simulator";
 document.title = gameName;
 
+let counter: number = 0;
+let startTime: number = 0;
+let growthRate: number = 0;
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🦟";
-app.append(button);
 
-let counter: number = 0;
+const upgrade: HTMLButtonElement = document.createElement("button");
+upgrade.innerHTML = "🎾";
+upgrade.disabled = true;
+
 const counterElement = document.createElement("div");
 counterElement.innerHTML =
   `${counter} 🦟` + (counter > 1 ? "s " : " ") + "swatted";
-app.append(counterElement);
 
-button.onclick = () => updateCounter();
+app.append(button);
+app.append(upgrade);
+app.append(counterElement);
 
 function updateCounter(amount: number = 1) {
   counter += amount;
+  const counterInteger: number = Math.round(counter);
   counterElement.innerHTML =
-    `${Math.round(counter)} 🦟` + (counter > 1 ? "s " : " ") + "swatted";
+    `${counterInteger} 🦟` + (counterInteger > 1 ? "s " : " ") + "swatted";
+  checkUnlock();
 }
 
-let lastTime = 0;
-
 function frameUpdate(currentTime: number) {
-  const delta = (currentTime - lastTime) / 1000;
+  const delta = (currentTime - startTime) / 1000;
 
-  lastTime = currentTime;
+  startTime = currentTime;
 
-  updateCounter(delta);
+  updateCounter(delta * growthRate);
 
   requestAnimationFrame(frameUpdate);
 }
 
-requestAnimationFrame(frameUpdate);
+function startTimeUpdate(currentTime: number) {
+  startTime = currentTime;
+  counter -= 10;
+
+  requestAnimationFrame(frameUpdate);
+}
+
+function checkUnlock() {
+  if (counter >= 10) {
+    upgrade.disabled = false;
+    growthRate = 1;
+  }
+}
+
+button.onclick = () => updateCounter();
+upgrade.onclick = () => requestAnimationFrame(startTimeUpdate);
+checkUnlock();
