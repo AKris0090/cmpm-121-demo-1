5c0abd5f79895997e83a64dc4f891f3e33a34703
Arjun Krishnan <akrish29@ucsc.edu>
2024-10-08 19:19:39 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "The Best Game Ever!!!";
+const gameName = "Mosquito Swatting Simulator";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🦟";
+app.append(button);
