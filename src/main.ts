18cf7bc7ba48534cd9a2386bd7b4d7cc2b6faa0f
Arjun Krishnan <akrish29@ucsc.edu>
2024-10-08 19:48:01 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,18 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
+const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🦟";
 app.append(button);
+
+let counter: number = 0;
+const counterElement = document.createElement("div");
+counterElement.innerHTML =
+  `${counter} 🦟` + (counter > 1 ? "s " : " ") + "swatted";
+app.append(counterElement);
+
+button.onclick = () => {
+  counter++;
+  counterElement.innerHTML =
+    `${counter} 🦟` + (counter > 1 ? "s " : " ") + "swatted";
+};
